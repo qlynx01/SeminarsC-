3346c00d293669d1a035c74dfe038bb6bc9213e4
Aidos <azhaynbai@gmail.com>
2023-02-16 15:42:15 +0600

    homework3

---
+++ homework3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = num % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
