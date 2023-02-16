3c3d51b7c836f26a7597a2b8966cfb171e592fe0
Aidos <azhaynbai@gmail.com>
2023-02-16 15:16:45 +0600

    add homework1 project

---
+++ homework1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber > secondNumber)
+{
+    Console.WriteLine("Первое число больше второго числа");
+}
+else
+{
+    Console.WriteLine("Второе число больше первого числа"); 
+}
+
