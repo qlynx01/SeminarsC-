f6c4d386886efe1342b5548d120b6a34eeb6821a
Aidos <azhaynbai@gmail.com>
2023-02-16 15:46:45 +0600

    homework4

---
+++ homework4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int evenNum = 2;
+
+if(num > 1)
+{
+    while(evenNum <= num)
+    {
+        Console.Write(evenNum + " ");
+        evenNum = evenNum + 2;
+    }
+}
